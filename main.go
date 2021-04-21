b7c6c005abedbd7b6d46cb8cef34ab065cd894ee
vyattalee <jie.n.li@nokia-sbell.com>
2021-04-21 21:58:07 +0800

    pipeline stage step for coffee shop model it is the first running version

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import (
 	"coffeeshop/dispatcher"
 	"coffeeshop/dispatcher1"
 	"coffeeshop/dispatcher2"
+	pipeline "coffeeshop/pipeline_model"
+	"context"
+	"io"
+	"io/ioutil"
+	"net/http"
+
 	//"coffeeshop/pipelineDispatcher"
 	//"coffeeshop/pipelineWorker"
 	"coffeeshop/worker"
@@ -14,28 +20,22 @@ import (
 	"time"
 )
 
-type Mammal interface {
-	Say()
+func main() {
+	//dispatch_worker()
+
+	//dispatch_worker1()
+
+	//dispatch_worker2()
+
+	//test4PolymorphicInheritanceBYInterface()
+	//pipelineDispatch_worker()
+
+	//pipeline_stage_step_model()
+	//
+	//simple_pipeline_model()
+
+	coffeeshop_pipeline_model()
 }
-
-type Dog struct{}
-
-type Cat struct{}
-
-type Human struct{}
-
-func (d Dog) Say() {
-	fmt.Println("woof")
-}
-
-func (c Cat) Say() {
-	fmt.Println("meow")
-}
-
-func (h Human) Say() {
-	fmt.Println("speak")
-}
-
 func test4PolymorphicInheritanceBYInterface() {
 	var m Mammal
 	var mammalchan chan Mammal
@@ -58,18 +58,6 @@ func test4PolymorphicInheritanceBYInterface() {
 		m.Say()
 	}
 }
-
-func main() {
-	//dispatch_worker()
-
-	//dispatch_worker1()
-
-	//dispatch_worker2()
-
-	//test4PolymorphicInheritanceBYInterface()
-	pipelineDispatch_worker()
-}
-
 func dispatch_worker() {
 	start := time.Now()
 	dd := dispatcher.New(10).Start()
@@ -306,4 +294,269 @@ func pipelineDispatch_worker() {
 
 	end := time.Now()
 	log.Print(end.Sub(start).Seconds())
+}
+
+type CoffeeStep struct {
+	pipeline.StepContext
+	Name   string
+	bytes  int64
+	fail   bool
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+func newStep(Name string, bytes int64, fail bool) *CoffeeStep {
+	ctx, cancel := context.WithCancel(context.Background())
+	d := &CoffeeStep{Name: Name, bytes: bytes, fail: fail}
+	d.ctx = ctx
+	d.cancel = cancel
+	return d
+}
+
+func (d *CoffeeStep) Exec(request *pipeline.Request) *pipeline.Result {
+
+	d.Status(fmt.Sprintf("%+v", request))
+
+	d.Status(fmt.Sprintf("Started %s", d.Name))
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://httpbin.org/bytes/%d", d.bytes), nil)
+	if err != nil {
+		return &pipeline.Result{Error: err}
+	}
+
+	req = req.WithContext(d.ctx)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return &pipeline.Result{Error: err}
+	}
+
+	defer resp.Body.Close()
+
+	n, err := io.Copy(ioutil.Discard, resp.Body)
+	if err != nil {
+		return &pipeline.Result{Error: err}
+	}
+
+	if d.fail {
+		return &pipeline.Result{Error: fmt.Errorf("File download failed %s", d.Name)}
+	}
+
+	d.Status(fmt.Sprintf("Successfully downloaded file %s", d.Name))
+
+	return &pipeline.Result{
+		Error:  nil,
+		Data:   struct{ bytesDownloaded int64 }{bytesDownloaded: n},
+		KeyVal: map[string]interface{}{"bytesDownloaded": n},
+	}
+}
+
+func (d *CoffeeStep) Cancel() error {
+	d.Status(fmt.Sprintf("Cancel downloading file %s", d.Name))
+	d.cancel()
+	return nil
+}
+
+func coffeeshop_pipeline_model() {
+	workflow := pipeline.NewProgress("coffee shop", 100, time.Second*5)
+
+	//stage
+	stage := pipeline.NewStage("stage1ForGrindBean&EspressoCoffee", false, false)
+
+	//steps
+	grindBeanStep := newStep("GrindBeanStep", 1, false)
+	espressoCoffeeStep := newStep("EspressoCoffeeStep", 2, false)
+
+	//stage with sequential steps
+	stage.AddStep(grindBeanStep, espressoCoffeeStep)
+
+	// add all stages
+	workflow.AddStage(stage)
+
+	// start a routine to read out and progress
+	go readPipeline(workflow)
+
+	// execute pipeline
+	result := workflow.Run()
+	if result.Error != nil {
+		fmt.Println(result.Error)
+	}
+
+	// one would persist the time taken duration to use as progress scale for the next workflow build
+	fmt.Println("timeTaken:", workflow.GetDuration())
+
+}
+
+func pipeline_stage_step_model() {
+	workflow := pipeline.NewProgress("getfiles", 1000, time.Second*7)
+	//stages
+	stage := pipeline.NewStage("stage", false, false)
+	// in this stage, steps will be executed concurrently
+	concurrentStage := pipeline.NewStage("con_stage", true, false)
+	// another concurrent stage
+	concurrentErrStage := pipeline.NewStage("con_err_stage", true, false)
+
+	//steps
+	fileStep1mb := newDownloadStep("1mbfile", 1e6, false)
+	fileStep1mbFail := newDownloadStep("1mbfileFail", 1e6, true)
+	fileStep5mb := newDownloadStep("5mbfile", 5e6, false)
+	fileStep10mb := newDownloadStep("10mbfile", 10e6, false)
+
+	//stage with sequential steps
+	stage.AddStep(fileStep1mb, fileStep5mb, fileStep10mb)
+
+	//stage with concurrent steps
+	concurrentStage.AddStep(fileStep1mb, fileStep5mb, fileStep10mb)
+
+	//stage with concurrent steps one of which fails early, prompting a cancellation
+	//of the other running steps.
+	concurrentErrStage.AddStep(fileStep1mbFail, fileStep5mb, fileStep10mb)
+
+	// add all stages
+	workflow.AddStage(stage, concurrentStage, concurrentErrStage)
+
+	// start a routine to read out and progress
+	go readPipeline(workflow)
+
+	// execute pipeline
+	result := workflow.Run()
+	if result.Error != nil {
+		fmt.Println(result.Error)
+	}
+
+	// one would persist the time taken duration to use as progress scale for the next workflow build
+	fmt.Println("timeTaken:", workflow.GetDuration())
+}
+
+type downloadStep struct {
+	pipeline.StepContext
+	fileName string
+	bytes    int64
+	fail     bool
+	ctx      context.Context
+	cancel   context.CancelFunc
+}
+
+func newDownloadStep(fileName string, bytes int64, fail bool) *downloadStep {
+	ctx, cancel := context.WithCancel(context.Background())
+	d := &downloadStep{fileName: fileName, bytes: bytes, fail: fail}
+	d.ctx = ctx
+	d.cancel = cancel
+	return d
+}
+
+func (d *downloadStep) Exec(request *pipeline.Request) *pipeline.Result {
+
+	d.Status(fmt.Sprintf("%+v", request))
+
+	d.Status(fmt.Sprintf("Started downloading file %s", d.fileName))
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://httpbin.org/bytes/%d", d.bytes), nil)
+	if err != nil {
+		return &pipeline.Result{Error: err}
+	}
+
+	req = req.WithContext(d.ctx)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return &pipeline.Result{Error: err}
+	}
+
+	defer resp.Body.Close()
+
+	n, err := io.Copy(ioutil.Discard, resp.Body)
+	if err != nil {
+		return &pipeline.Result{Error: err}
+	}
+
+	if d.fail {
+		return &pipeline.Result{Error: fmt.Errorf("File download failed %s", d.fileName)}
+	}
+
+	d.Status(fmt.Sprintf("Successfully downloaded file %s", d.fileName))
+
+	return &pipeline.Result{
+		Error:  nil,
+		Data:   struct{ bytesDownloaded int64 }{bytesDownloaded: n},
+		KeyVal: map[string]interface{}{"bytesDownloaded": n},
+	}
+}
+
+func (d *downloadStep) Cancel() error {
+	d.Status(fmt.Sprintf("Cancel downloading file %s", d.fileName))
+	d.cancel()
+	return nil
+}
+
+func readPipeline(pipe *pipeline.Pipeline) {
+	out, err := pipe.Out()
+	if err != nil {
+		return
+	}
+
+	progress, err := pipe.GetProgressPercent()
+	if err != nil {
+		return
+	}
+
+	for {
+		select {
+		case line := <-out:
+			fmt.Println(line)
+		case p := <-progress:
+			fmt.Println("percent done: ", p)
+		}
+	}
+}
+
+/////////////////////////following simple example////////////////////////
+
+func simple_pipeline_model() {
+
+	workpipe := pipeline.NewProgress("myProgressworkpipe", 1000, time.Second*3)
+	stage := pipeline.NewStage("mypworkstage", false, false)
+	step1 := &work{id: 1}
+	step2 := &work{id: 2}
+
+	stage.AddStep(step1)
+	stage.AddStep(step2)
+
+	workpipe.AddStage(stage)
+
+	go readPipeline(workpipe)
+
+	result := workpipe.Run()
+	if result.Error != nil {
+		fmt.Println(result.Error)
+	}
+
+	fmt.Println("timeTaken:", workpipe.GetDuration())
+}
+
+type work struct {
+	pipeline.StepContext
+	id int
+}
+
+func (w *work) Exec(request *pipeline.Request) *pipeline.Result {
+
+	w.Status(fmt.Sprintf("%+v", request))
+	duration := time.Duration(1000 * w.id)
+	time.Sleep(time.Millisecond * duration)
+	msg := fmt.Sprintf("work %d", w.id)
+	return &pipeline.Result{
+		Error:  nil,
+		Data:   map[string]string{"msg": msg},
+		KeyVal: map[string]interface{}{"msg": msg},
+	}
+}
+
+func (w *work) Cancel() error {
+	w.Status("cancel step")
+	return nil
 }
